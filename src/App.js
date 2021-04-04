76b4d1e708d10d19d7512bbb9ed8673939ccee28
Rokas <rookas.rudzenskas@gmail.com>
2021-04-04 09:36:44 +0300

    Buttons

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from "react";
 
 const App = () => {
     return (
-        <>
-        <h1>Welcome to React Parcel Micro App!</h1>
-        <p>Hard to get more minimal than this React app.</p>
+        <div>
+        <h1>TIC TAC TOE</h1>
 
-    </>
+
+    </div>
     );
 };
 
---
+++ src/components/Board.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Board = () => {
+    return (
+        <div>
+            <div>
+                <button></button>
+                <button></button>
+                <button></button>
+            </div>
+            <div>
+                <button></button>
+                <button></button>
+                <button></button>
+            </div>
+            <div>
+                <button></button>
+                <button></button>
+                <button></button>
+            </div>
+        </div>
+    )
+}
+
+export default Board
