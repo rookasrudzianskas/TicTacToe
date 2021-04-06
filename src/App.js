62521abb44a60487ed2965f21698aee91ffe098c
Rokas <rookas.rudzenskas@gmail.com>
2021-04-06 09:49:56 +0300

    First

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Board from "./components/Board";
 import History from "./components/History";
+import StatusMessage from "./components/StatusMessage";
 
 import "./styles/root.scss"
 import {calculateWinner} from "./helpers";
@@ -15,8 +16,6 @@ const App = () => {
     console.log('history', history);
 
     const winner = calculateWinner(current.board);
-
-    const message = winner ? `Winner is ${winner}` : `Next player is ${current.isXNext ? 'X' : '0'}`
 
     // console.log(winner);
 
@@ -46,7 +45,7 @@ const App = () => {
     return (
         <div className="app">
         <h1>TIC TAC TOE</h1>
-            <h2>{ message }</h2>
+            <StatusMessage winner={winner} current={current}/>
             <Board board={current.board} handleSquareClick={handleSquareClick}/>
             <History history={history} moveTo={moveTo} currentMove={currentMove}/>
 
