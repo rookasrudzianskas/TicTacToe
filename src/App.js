174c71ef846832a8a4aa6dd1191a94c60b20bf76
Rokas <rookas.rudzenskas@gmail.com>
2021-04-06 10:10:18 +0300

    Styling

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ const App = () => {
 
     return (
         <div className="app">
-        <h1>TIC TAC TOE</h1>
+        <h1>TIC <span className="text-green">TAC</span>  TOE</h1>
             <StatusMessage winner={winner} current={current}/>
             <Board board={current.board} handleSquareClick={handleSquareClick} winningSquares={winningSquares}/>
             <button type="button" onClick={onNewGame}>Start new game</button>
